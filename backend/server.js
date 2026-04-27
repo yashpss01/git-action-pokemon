e4ad46f8713ac9c24b6e97ec756b4ef2bccc006b
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2026-04-27 10:09:44 +0530

    changes in file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 
+const router = require('./routes/index')
+app.use(router)
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
